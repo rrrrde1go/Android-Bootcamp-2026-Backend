5e9bd2e0a7b28294d7a6eb1959e9c24bc51a0f40
Vova1707 <kondrahinvov@yandex.ru>
2026-02-07 17:27:02 +0300

    Исправлено ФИО пользователя и добавлена проверка БД

--- src/main/java/ru/sicampus/bootcamp2026/App.java
+++ src/main/java/ru/sicampus/bootcamp2026/App.java
@@ -3,9 +3,24 @@ package ru.sicampus.bootcamp2026;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import ru.sicampus.bootcamp2026.repository.UserRepository;
+
 @SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(UserRepository userRepository) {
+        return args -> {
+            System.out.println("Checking users in DB:");
+            userRepository.findAll().forEach(user -> {
+                    System.out.println("User: " + user.getEmail() + " | Name: " + user.getFullName());
+                });
+        };
     }
 }
